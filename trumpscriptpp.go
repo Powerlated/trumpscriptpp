92824a3f79bb5ee5161ed0fd08ca00f0648e2fb1
Powerlated <zjia08@gmail.com>
2018-02-16 00:00:51 -0500

    moved the println up to make it more aethestically pleasing

--- trumpscriptpp.go
+++ trumpscriptpp.go
@@ -16,11 +16,11 @@ var hillaryscriptpp = false
 func main() {
 	color.Red("Welcome to TrumpScript++!")
 
+	fmt.Println()
+
 	fmt.Printf(color.BlueString("To switch to HillaryScript++ mode, type "))
 	fmt.Printf(color.YellowString("EMAILS EMAILS EMAILS"))
 	fmt.Println(color.BlueString(" in full caps."))
-
-	fmt.Println()
 
 	fmt.Printf(color.BlueString("To continue with TrumpScript++ mode, type "))
 	fmt.Printf(color.YellowString("PRESIDENT OF THE VIRGIN ISLANDS"))
