45440f60afb1f3cce7ae4ede1ea8af307babd4ab
Powerlated <zjia08@gmail.com>
2018-02-15 23:34:29 -0500

    added more president ridicule

--- trumpscriptpp.go
+++ trumpscriptpp.go
@@ -12,4 +12,10 @@ func main() {
 	fmt.Printf(color.BlueString("To switch to HillaryScript++ mode, type "))
 	fmt.Printf(color.YellowString("\"EMAILS EMAILS EMAILS\""))
 	fmt.Println(color.BlueString(" in full caps."))
+
+	fmt.Println()
+
+	fmt.Printf(color.BlueString("To continue with TrumpScript++ mode, type "))
+	fmt.Printf(color.YellowString("\"PRESIDENT OF THE VIRGIN ISLANDS\""))
+	fmt.Println(color.BlueString(" in full caps."))
 }
