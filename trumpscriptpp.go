8f82e311f516bae083cd43dd04ec9cba8bc0da1a
Powerlated <zjia08@gmail.com>
2018-02-15 23:21:21 -0500

    Smarter print positioning

--- trumpscriptpp.go
+++ trumpscriptpp.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fatih/color"
 )
 
 func main() {
 	color.Red("Welcome to TrumpScript++!")
-	color.Blue("To switch to HillaryScript++ mode, type \"EMAILS EMAILS EMAILS\" in full caps.")
+	fmt.Printf(color.BlueString("To switch to HillaryScript++ mode, type "))
+	fmt.Printf(color.YellowString("\"EMAILS EMAILS EMAILS\""))
+	fmt.Println(color.BlueString(" in full caps."))
 }
