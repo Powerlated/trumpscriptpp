61fb5b10114201f53004daa4f73447728082ec13
Powerlated <zjia08@gmail.com>
2018-02-15 23:47:36 -0500

    isTrumpSupporter smart answers

--- trumpscriptpp.go
+++ trumpscriptpp.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	survey "gopkg.in/AlecAivazis/survey.v1"
@@ -33,6 +35,19 @@ func isTrumpSupporter() bool {
 			Message: "Of which mode do you want to enter?",
 		}
 		survey.AskOne(prompt, &answer, nil)
+
+		if strings.ToLower(answer) == "quit" {
+			os.Exit(0)
+		} else if answer == "EMAILS EMAILS EMAILS" {
+			fmt.Println(color.BlueString("ALL HAIL OUR LOW IQ GENIUS HILLARY CLINTON AND HER PRIVATE EMAIL SERVERS!"))
+			statusConfirmed = true
+		} else if answer == "PRESIDENT OF THE VIRGIN ISLANDS" {
+			fmt.Println(color.RedString("ALL HAIL OUR 70 YEAR OLD PRESIDENT IN ACCELERATED READING!"))
+			statusConfirmed = true
+		} else {
+			fmt.Println(color.GreenString("THAT IS NOT AN OPTION! SCREW OFF COMMUNIST PIGS!"))
+			fmt.Println()
+		}
 	}
 	return false
 }
