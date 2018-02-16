609b4e536ce7f88b91a41e55bd5909fc11881a9e
Powerlated <zjia08@gmail.com>
2018-02-15 22:58:57 -0500

    Added colored instructions on how to use the program

--- trumpscriptpp.go
+++ trumpscriptpp.go
@@ -1,5 +1,10 @@
 package trumpscriptpp
 
-func main() {
+import (
+	"github.com/fatih/color"
+)
 
+func main() {
+	color.Blue("Welcome to TrumpScript++!")
+	color.Red("To switch to HillaryScript++ mode, type \"EMAILS EMAILS EMAILS\" in full caps.")
 }
