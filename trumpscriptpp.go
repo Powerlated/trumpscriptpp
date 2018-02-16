ca0dbe9c8f4ab1c38c6c604239e79afb2b04bb27
Powerlated <zjia08@gmail.com>
2018-02-16 00:00:33 -0500

    removed quotation marks from switch messages

--- trumpscriptpp.go
+++ trumpscriptpp.go
@@ -17,13 +17,13 @@ func main() {
 	color.Red("Welcome to TrumpScript++!")
 
 	fmt.Printf(color.BlueString("To switch to HillaryScript++ mode, type "))
-	fmt.Printf(color.YellowString("\"EMAILS EMAILS EMAILS\""))
+	fmt.Printf(color.YellowString("EMAILS EMAILS EMAILS"))
 	fmt.Println(color.BlueString(" in full caps."))
 
 	fmt.Println()
 
 	fmt.Printf(color.BlueString("To continue with TrumpScript++ mode, type "))
-	fmt.Printf(color.YellowString("\"PRESIDENT OF THE VIRGIN ISLANDS\""))
+	fmt.Printf(color.YellowString("PRESIDENT OF THE VIRGIN ISLANDS"))
 	fmt.Println(color.BlueString(" in full caps."))
 
 	isTrumpSupporter()
